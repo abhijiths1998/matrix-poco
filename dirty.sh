821353ef509e459ac05aa6b02dd78a2dc86f3ea6
abhijiths0098 <abhivarma362@gmail.com>
2019-11-18 16:46:31 +0000

    Compilation scripts

--- dirty.sh
+++ dirty.sh
@@ -8,15 +8,13 @@ GCC="/home/abhivarma362/gcc64/bin/aarch64-elf-"
 GCC32="/home/abhivarma362/gcc32/bin/arm-maestro-linux-gnueabi-"
 CLANG_PATH="/home/abhivarma362/clang/bin/clang"
 
-mkdir -p out
-
-make clean
-make mrproper
 
 make O=out ARCH=arm64 beryllium_defconfig
 
 make -j$(nproc --all) O=out ARCH=arm64 \
                         CC="${CLANG_PATH}" \
                         CLANG_TRIPLE="aarch64-linux-gnu-" \
-			CROSS_COMPILE="${GCC}" \
-			CROSS_COMPILE_ARM32="${GCC32}"
+                        CROSS_COMPILE="${GCC}" \
+                        CROSS_COMPILE_ARM32="${GCC32}"
+
+
