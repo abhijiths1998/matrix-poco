b5df2b911d93f516a62c5ef8dcebb3914a342d88
abhijiths0098 <abhivarma362@gmail.com>
2019-11-20 05:28:21 +0000

    Revert "cpu_input_boost: Mark boost kthread as performance critical"
    This reverts commit ed7d6aa5726c7d474401d1ff4283702eb6130959.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -406,14 +406,7 @@ static int __init cpu_input_boost_init(void)
 		goto unregister_handler;
 	}
 
-	boost_thread = kthread_run_perf_critical(cpu_boost_thread, b,
-						 "cpu_boostd");
-	if (IS_ERR(boost_thread)) {
-		pr_err("Failed to start CPU boost thread, err: %ld\n",
-		       PTR_ERR(boost_thread));
-		goto unregister_drm_notif;
-	}
-
+	/* Allow global boost config access for external boosts */
 	boost_drv_g = b;
 
 	return 0;
