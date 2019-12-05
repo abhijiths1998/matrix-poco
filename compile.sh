f244082c8d7cea7029f0ef479f02f72c511d22c9
abhivarma362 <abhijiths0098@gmail.com>
2019-12-05 10:55:37 +0000

    caf mismerge and some script changes

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="abhijiths0098"
 export KBUILD_BUILD_HOST="matrix"
-GCC="/home/abhivarma362/gcc64/bin/aarch64-elf-"
-GCC32="/home/abhivarma362/gcc32/bin/arm-maestro-linux-gnueabi-"
-CLANG_PATH="/home/abhivarma362/clang/bin/clang"
+GCC="/home/abhivarma362/disk/gcc64/bin/aarch64-maestro-linux-gnu-"
+GCC32="/home/abhivarma362/disk/gcc32/bin/arm-maestro-linux-gnueabi-"
+CLANG_PATH="/home/abhivarma362/disk/clang/bin/clang"
 
 mkdir -p out
 
@@ -17,8 +17,9 @@ make O=out ARCH=arm64 matrix_defconfig
 
 make -j$(nproc --all) O=out ARCH=arm64 \
                         CC="${CLANG_PATH}" \
-                        CLANG_TRIPLE="aarch64-linux-gnu-" \
+                        CLANG_TRIPLE="aarch64-maestro-linux-gnu-" \
 			CROSS_COMPILE="${GCC}" \
 			CROSS_COMPILE_ARM32="${GCC32}"
 
-mv out/arch/arm64/boot/Image.gz-dtb ../AnyKernel3
+mv out/arch/arm64/boot/Image.gz-dtb ../anykernel
+
